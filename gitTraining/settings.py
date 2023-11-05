77e92c81c022289f8db7fdff80c2891e017fb0df
sv0ice <sakurav0iceee@gmail.com>
2023-11-05 23:27:34 +0600

    add sqlite and change En to Ru settings

--- gitTraining/settings.py
+++ gitTraining/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
