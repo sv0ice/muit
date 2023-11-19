24d5beadcaa42a8dfa8b383de7ad42eb7629c3c7
sv0ice <sakurav0iceee@gmail.com>
2023-11-19 18:06:28 +0600

    update models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,29 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    # class GroupManager(models.Manager):
+    #     """
+    #     The manager for the auth's Group model.
+    #     """
+    #
+    #     use_in_migrations = True
+    #
+    #     def get_by_natural_key(self, name):
+    #         return self.get(name=name)
+
+    objects = models.Manager()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # settings.AUTH_USER_MODEL
+    answer1 = models.TextField(default="Не выполнено!")
+    answer2 = models.TextField(default="Не выполнено!")
+    answer3 = models.TextField(default="Не выполнено!")
+    answer4 = models.TextField(default="Не выполнено!")
+    answer5 = models.TextField(default="Не выполнено!")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 #
 # class Lesson(models.Model):
 #     title = models.CharField(max_length=255)
@@ -6,9 +31,3 @@
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now=True)
 #
-# class Question(models.Model):
-#     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-#     prompt = models.CharField(max_length=255)
-#     answer = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
